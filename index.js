ef1e89c30745b01d61c9c4220ed3d60fafd7156c
prasanta1987 <prasanta.1987@hotmail.com>
2020-07-12 01:19:03 +0530

    updated

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const csvtojson = require('csvtojson')
 
 const fbConfig = require('./fbConfig').firebaseConfig
 
+let userAction = process.argv[2].toLowerCase()
+let isUpload = process.argv[4].toLowerCase() || false
+
 const url = 'https://www1.nseindia.com/products/content/sec_bhavdata_full.csv'
 
 firebase.initializeApp(fbConfig)
@@ -66,12 +69,17 @@ const makeJsonObject = (stockData) => {
             [headerRow[14]]: parseFloat(x[14]),
         }
 
-        storeData(data, i)
-        i++;
+        if (isUpload == 'uplaod') {
+            storeData(data, i)
+            i++;
+        }
         jsonArrayData.push(data)
     })
-    console.log(jsonArrayData)
-    // storeToFile(jsonArrayData)
+
+    if (isUpload == false) {
+        console.log(jsonArrayData)
+    }
+    storeToFile(jsonArrayData)
 }
 
 
@@ -82,7 +90,7 @@ const storeData = (data, i) => {
 
     stockDbData.doc(symbol).set({ [date]: data }, { merge: true })
         .then(() => {
-            console.log(`${i} : "${symbol}" dated "${date}" is Saved`)
+            console.log(`${i} : "${symbol}" dated "${timeStamptoDate(date)}" is Saved`)
         })
         .catch(() => console.log(`${i} : ${symbol} dated ${date} Failed to Save`))
 }
@@ -98,6 +106,24 @@ const convertDateToTimeStamp = (date) => {
     let newDate = new Date(date).getTime()
     return newDate
 }
+
+const timeStamptoDate = (time) => {
+
+    let d = new Date(time)
+    let day = d.getDate()
+    let mon = d.getMonth()
+    let year = d.getFullYear()
+
+    var monthNames = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+
+    let date = `${day}-${monthNames[mon]}-${year}`
+
+    return date
+}
+
 
 let userAction = process.argv[2].toLowerCase()
 
