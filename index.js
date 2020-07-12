4363c9b9310d79fd4a5cd14775c8aa70cc81c755
prasanta1987 <prasanta.1987@hotmail.com>
2020-07-12 11:06:24 +0530

    canges

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ const stockDbData = db.collection('stockHistoricalData')
 
 const getData = () => {
     axios.get(url)
-        .then(res => convertDatas(res.data))
+        .then(res => {
+            convertDatas(res.data)
+            let csvDate = res.data.split('\n')[2].split(',')[2].trim()
+            storeToFile(res.data, csvDate)
+
+        })
         .catch(() => console.log('Error in Fetching Data'))
 }
 
@@ -37,69 +42,74 @@ const convertDatas = (data) => {
 
     csv({ noheader: true, output: "csv" })
         .fromString(data)
-        .then((stockData) => {
-
-            makeJsonObject(stockData)
-
-        })
+        .then((stockData) => makeJsonObject(stockData))
         .catch(() => console.log('Conversion Error'))
 }
 
 const makeJsonObject = (stockData) => {
 
     let headerRow = stockData.shift()
+    let csvDate = stockData[2][2]
     let jsonArrayData = []
     let i = 1
     stockData.map(x => {
-        let data = {
-            [headerRow[0]]: x[0],
-            [headerRow[1]]: x[1],
-            [headerRow[2]]: (isUpload) ? convertDateToTimeStamp(x[2]) : x[2],
-            [headerRow[3]]: parseFloat(x[3]),
-            [headerRow[4]]: parseFloat(x[4]),
-            [headerRow[5]]: parseFloat(x[5]),
-            [headerRow[6]]: parseFloat(x[6]),
-            [headerRow[7]]: parseFloat(x[7]),
-            [headerRow[8]]: parseFloat(x[8]),
-            [headerRow[9]]: parseFloat(x[9]),
-            [headerRow[10]]: parseFloat(x[10]),
-            [headerRow[11]]: parseFloat(x[11]),
-            [headerRow[12]]: parseFloat(x[12]),
-            [headerRow[13]]: parseFloat(x[13]),
-            [headerRow[14]]: parseFloat(x[14]),
-        }
+        if (x[1] == 'EQ') {
 
-        if (isUpload) {
-            storeData(data, i)
-            i++;
+            let data = {
+                [headerRow[0]]: x[0],
+                [headerRow[1]]: x[1],
+                [headerRow[2]]: (isUpload) ? convertDateToTimeStamp(x[2]) : x[2],
+                [headerRow[3]]: parseFloat(x[3]),
+                [headerRow[4]]: parseFloat(x[4]),
+                [headerRow[5]]: parseFloat(x[5]),
+                [headerRow[6]]: parseFloat(x[6]),
+                [headerRow[7]]: parseFloat(x[7]),
+                [headerRow[8]]: parseFloat(x[8]),
+                [headerRow[9]]: parseFloat(x[9]),
+                [headerRow[10]]: parseFloat(x[10]),
+                [headerRow[11]]: parseFloat(x[11]),
+                [headerRow[12]]: parseFloat(x[12]),
+                [headerRow[13]]: parseFloat(x[13]),
+                [headerRow[14]]: parseFloat(x[14]),
+            }
+
+            jsonArrayData.push(data)
+
+            if (isUpload) {
+                storeData(data, i)
+                i++;
+            }
         }
-        jsonArrayData.push(data)
     })
 
     if (isUpload == false) {
-        console.log(jsonArrayData)
+        console.log(`Report for ${csvDate} is available`)
     }
-    storeToFile(jsonArrayData)
 }
 
+const storeToFile = (data, date) => {
+    try {
+        fs.writeFileSync(`./offlineCsv/${date}.csv`, data)
+        console.log('File Written Successfully')
+    } catch (error) {
 
-const storeData = (data, i) => {
+        console.log('File Save Error', error)
+
+    }
+}
+
+const storeData = async (data, i) => {
 
     let symbol = data.SYMBOL
     let date = data.DATE1
 
-    stockDbData.doc(symbol).set({ [date]: data }, { merge: true })
-        .then(() => {
-            console.log(`${i} : "${symbol}" dated "${timeStamptoDate(date)}" is Saved`)
-        })
-        .catch(() => console.log(`${i} : ${symbol} dated ${date} Failed to Save`))
-}
+    try {
+        await stockDbData.doc(symbol).set({ [date]: data }, { merge: true })
+        console.log(`${i} : "${symbol}" dated "${timeStamptoDate(date)}" is Saved`)
 
-const storeToFile = (jsonArrayData) => {
-
-    let fileDate = jsonArrayData[0].DATE1
-    fs.writeFileSync(`./offlineData/stock_data_${fileDate}.json`, JSON.stringify(jsonArrayData))
-
+    } catch (error) {
+        console.log(`${i} : ${symbol} dated ${date} Failed to Save`)
+    }
 }
 
 const convertDateToTimeStamp = (date) => {
