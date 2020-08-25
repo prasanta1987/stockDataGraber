e6dfedfd07eb0d231c90d9c6306c748f4c0cc30a
prasanta1987 <prasanta.1987@hotmail.com>
2020-08-25 14:35:07 +0530

    updated

--- fbConfig.js
+++
@@ -1,9 +0,0 @@
-exports.firebaseConfig = {
-    apiKey: "AIzaSyBp9j1irJlYlTzu0NhrJe4ngyxDXi4HCH4",
-    authDomain: "pran-home.firebaseapp.com",
-    databaseURL: "https://pran-home.firebaseio.com",
-    projectId: "pran-home",
-    storageBucket: "pran-home.appspot.com",
-    messagingSenderId: "802658990350",
-    appId: "1:802658990350:web:248bfdad28bc7a7c657f5f"
-};
--- index.js
+++ index.js
@@ -1,116 +1,72 @@
 const axios = require('axios')
 const csv = require('csvtojson')
-const firebase = require('firebase')
 const fs = require('fs')
-const csvtojson = require('csvtojson')
+const moment = require('moment')
+const { convertArrayToCSV } = require('convert-array-to-csv');
 
-const fbConfig = require('./fbConfig').firebaseConfig
+let userAction = process.argv
+userAction.shift()
+userAction.shift()
 
-let userAction = process.argv[2].toLowerCase()
-let isUpload = process.argv[4] || false
+const type = userAction[0].toLocaleUpperCase()
+const symbol = userAction[1].toUpperCase()
+const series = userAction[2].toUpperCase()
+const startDate = userAction[3]
+const endDate = userAction[4]
 
-const url = 'https://www1.nseindia.com/products/content/sec_bhavdata_full.csv'
-
-firebase.initializeApp(fbConfig)
-const db = firebase.firestore()
-const stockDbData = db.collection('stockHistoricalData')
-
-
-const getData = () => {
-    axios.get(url)
-        .then(res => {
-            convertDatas(res.data)
-            let csvDate = res.data.split('\n')[2].split(',')[2].trim()
-            storeToFile(res.data, csvDate)
-
-        })
-        .catch(() => console.log('Error in Fetching Data'))
-}
-
-
-const convertFromFile = (fileName = 'sec_bhavdata_full.csv') => {
-
-    csv({ noheader: true, output: "csv" })
-        .fromFile(`./offlineCsv/${fileName}`)
-        .then(jsonObj => {
-            makeJsonObject(jsonObj)
-        })
-        .catch(() => console.log('File Name Incorrect.'))
-}
-
-const convertDatas = (data) => {
-
-    csv({ noheader: true, output: "csv" })
-        .fromString(data)
-        .then((stockData) => makeJsonObject(stockData))
-        .catch(() => console.log('Conversion Error'))
-}
-
-const makeJsonObject = (stockData) => {
-
-    let headerRow = stockData.shift()
-    let csvDate = stockData[2][2]
-    let jsonArrayData = []
-    let i = 1
-    stockData.map(x => {
-        if (x[1] == 'EQ') {
-
-            let data = {
-                [headerRow[0]]: x[0],
-                [headerRow[1]]: x[1],
-                [headerRow[2]]: (isUpload) ? convertDateToTimeStamp(x[2]) : x[2],
-                [headerRow[3]]: parseFloat(x[3]),
-                [headerRow[4]]: parseFloat(x[4]),
-                [headerRow[5]]: parseFloat(x[5]),
-                [headerRow[6]]: parseFloat(x[6]),
-                [headerRow[7]]: parseFloat(x[7]),
-                [headerRow[8]]: parseFloat(x[8]),
-                [headerRow[9]]: parseFloat(x[9]),
-                [headerRow[10]]: parseFloat(x[10]),
-                [headerRow[11]]: parseFloat(x[11]),
-                [headerRow[12]]: parseFloat(x[12]),
-                [headerRow[13]]: parseFloat(x[13]),
-                [headerRow[14]]: parseFloat(x[14]),
-            }
-
-            jsonArrayData.push(data)
-
-            if (isUpload) {
-                storeData(data, i)
-                i++;
-            }
-        }
-    })
-
-    if (isUpload == false) {
-        console.log(`Report for ${csvDate} is available`)
-    }
-}
-
-const storeToFile = (data, date) => {
-    try {
-        fs.writeFileSync(`./offlineCsv/${date}.csv`, data)
-        console.log('File Written Successfully')
-    } catch (error) {
-
-        console.log('File Save Error', error)
-
-    }
-}
-
-const storeData = async (data, i) => {
-
-    let symbol = data.SYMBOL
-    let date = data.DATE1
+const fetchHistoricalData = async (symbol, series, startDate, endDate) => {
 
     try {
-        await stockDbData.doc(symbol).set({ [date]: data }, { merge: true })
-        console.log(`${i} : "${symbol}" dated "${timeStamptoDate(date)}" is Saved`)
+        let symb = symbol.toLowerCase()
+        symb = symb.replace('&', '%26')
+        const url = `https://www.nseindia.com/api/historical/cm/equity?symbol=${symb}&series=["${series}"]&from=${startDate}&to=${endDate}`
+        let cumData = []
+        let data = await axios.get(url)
 
-    } catch (error) {
-        console.log(`${i} : ${symbol} dated ${date} Failed to Save`)
+        return data.data.data
+
+    } catch (err) {
+        console.log(err.response.data.message)
+        // setTimeout(() => fetchHistoricalData(symbol, series, startDate, endDate), 5000)
     }
 }
+
+const get1stHistoricalData = (symbol, series) => {
+
+    const toDate = moment().format('DD-MM-yyyy')
+    const fromDate = moment().subtract(100, 'days').format('DD-MM-yyyy')
+
+    fetchHistoricalData(symbol, series, fromDate, toDate)
+        .then(data => {
+            getFinalData(symbol, series, data)
+        })
+}
+
+const getFinalData = (symbol, series, cumData) => {
+
+    let lastDate = cumData[cumData.length - 1].mTIMESTAMP
+    const toDate = moment(new Date(lastDate).getTime()).subtract(1, 'days').format('DD-MM-yyyy')
+    const fromDate = moment(new Date(lastDate).getTime()).subtract(100, 'days').format('DD-MM-yyyy')
+
+    console.log(`Fetched data upto ${cumData[cumData.length - 1].mTIMESTAMP}`)
+    fetchHistoricalData(symbol, series, fromDate, toDate)
+        .then(data => {
+            cumData = cumData.concat(data)
+            if (data.length > 2) {
+                getFinalData(symbol, series, cumData)
+            } else {
+                const csvData = convertArrayToCSV(cumData);
+                fs.writeFileSync(`./output/${symbol}-historical-data.csv`, csvData)
+            }
+        })
+
+}
+
+
+if (type == 'HISTORICALDATA') {
+    get1stHistoricalData(symbol, series, startDate, endDate)
+}
+
 
 const convertDateToTimeStamp = (date) => {
     let newDate = new Date(date).getTime()
@@ -128,19 +84,11 @@ const timeStamptoDate = (time) => {
         "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
     ];
 
-
     let date = `${day}-${monthNames[mon]}-${year}`
-
     return date
 }
 
-if (userAction == 'online') {
-    getData()
-}
-else if (userAction == 'offline') {
-    if (process.argv.length >= 3) {
-        convertFromFile(process.argv[3])
-    } else {
-        convertFromFile()
-    }
+const sanitizeInputs = (value) => {
+    value = parseFloat(value)
+    if (isNaN(value)) { return 0 } else { return value }
 }
